4ed1f044238160d815e946272428e4aecee1ca08
sh1nome <shinodayo1024@gmail.com>
2025-05-15 00:47:51 +0900

    pack.shの処理を簡素化し、generate_jail.mcfunctionとmanifest.jsonを追加

--- behavior-packs/pack.sh
+++ behavior-packs/pack.sh
@@ -12,16 +12,8 @@ else
     mkdir "$DIST_DIR"
 fi
 
-# src直下のディレクトリごとに処理
-for dir in "$SRC_DIR"/*/; do
-    # ディレクトリ名のみ取得
-    pack_name=$(basename "$dir")
-    # ディレクトリ内のファイル・ディレクトリのみをzip化（src/を含めない）
-    (cd "$dir" && zip -r "../${pack_name}.zip" .)
-    # 拡張子を.mcpackに変更
-    mv "$SRC_DIR/${pack_name}.zip" "${pack_name}.mcpack"
-    # distに移動
-    mv "${pack_name}.mcpack" "$DIST_DIR/"
-done
+# srcディレクトリの中身のみをzip化し、拡張子を.mcpackに変更
+(cd "$SRC_DIR" && zip -r "../$DIST_DIR/admin-commands.zip" .)
+mv "$DIST_DIR/admin-commands.zip" "$DIST_DIR/admin-commands.mcpack"
 
-echo "すべてのパックがzip化され、$DIST_DIR に移動されました。"
+echo "$SRC_DIR 以下が admin-commands.mcpack として $DIST_DIR に作成されました。"
